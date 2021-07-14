085ac93428b4f7d976d2133a1a879c3fdd21f62d
heeja6752 <43363703+heeja6752@users.noreply.github.com>
2021-07-14 20:18:46 +0900

    Update 6.연산자.py

--- 6.연산자.py
+++ 6.연산자.py
@@ -17,3 +17,15 @@ print(5<=5) #True
 print(3==3) #True
 print(4==2) #False
 print(3+4==7) #True
+
+print(1 != 3) #다를때 True
+print(not (1 != 3)) #False
+
+print((3>0) and (3<5)) #True
+print((3>0) & (3<5)) #True
+
+print((3>0) or (3>5)) #True
+print((3>0) | (3>5)) #True
+
+print(5>4>3) #True
+print(5>4>7) #False
