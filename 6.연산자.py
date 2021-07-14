7c5cb10dc45099a1bfa4834473ff6c9edee67b10
heeja6752 <43363703+heeja6752@users.noreply.github.com>
2021-07-14 20:14:15 +0900

    Create 6.연산자.py

---
+++ 6.연산자.py
@@ -0,0 +1,19 @@
+print(1+1) #2
+print(3-2) #1
+print(5*2) #10
+print(6/3) #2
+
+print(2**3) #2^3 = 8
+print(5%3) #나머지2
+print(10%3) #나머지1
+print(5//3) #몫1
+print(10//3) #몫3
+
+print(10>3) #True
+print(4>=7) #False
+print(10<3) #False
+print(5<=5) #True
+
+print(3==3) #True
+print(4==2) #False
+print(3+4==7) #True
