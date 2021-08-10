66ae3f6851630eadaa29b8cfd93f8d22ed772a9f
heeja6752 <43363703+heeja6752@users.noreply.github.com>
2021-08-10 15:00:06 +0900

    Update 25.while.py

--- 25.while.py
+++ 25.while.py
@@ -14,3 +14,9 @@
 #     print("{0}, 커피가 준비 되었습니다. 호출 {1} 회".format(customer, index))
 #     index +=1
 
+customer = "토르"
+person = "Unknown"
+
+while person != customer :
+    print("{0}, 커피가 준비 되었습니다.".format(customer))
+    person = input("이름이 어떻게 되세요?") 
